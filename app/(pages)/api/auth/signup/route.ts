0e830e930f6063f9af3a722e4a5e4afa792dc253
Anna Zoi <zoianna01@gmail.com>
2025-08-11 23:16:11 +0300

    uuid

--- app/(pages)/api/auth/signup/route.ts
+++ app/(pages)/api/auth/signup/route.ts
@@ -1,9 +1,11 @@
 import pool from "@/lib/db";
-import bcrypt from "bcrypt"; // for password hashing
+import bcrypt from "bcrypt";
+import { v4 as uuidv4 } from "uuid";
 
 export async function POST(request: Request) {
   try {
     const { email, password } = await request.json();
+    const id = uuidv4();
 
     if (!email || !password) {
       return Response.json(
@@ -12,15 +14,14 @@ export async function POST(request: Request) {
       );
     }
 
-    // Hash password before storing
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const result = await pool.query(
-      'INSERT INTO "users" (email, password) VALUES ($1, $2) RETURNING id, email',
-      [email, hashedPassword]
+      'INSERT INTO "users" (uuid, email, password) VALUES ($1, $2, $3) RETURNING uuid, email',
+      [id, email, hashedPassword]
     );
 
-    return Response.json(result.rows[0]); // Return the created user (without password)
+    return Response.json(result.rows[0]); // (returned without password)
   } catch (err: unknown) {
     return Response.json({ error: (err as Error).message }, { status: 500 });
   }
